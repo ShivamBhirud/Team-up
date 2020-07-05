78d28cc54e2f9e4aeb67ab921a5685ee92499368
ShivamBhirud <shivambhirudpr@gmail.com>
2020-07-05 22:47:52 +0530

    added a join button on t-up advertisement, yet to be made functional

--- team_up/models.py
+++ team_up/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from accounts.models import extendeduser
 # Create your models here.
 
 class Teams(models.Model):
   category = models.CharField(max_length=100)
   relevant_url = models.URLField(default='Not Provided')
   pub_date = models.DateTimeField(null=True, blank=True)
+  # team_id = models.AutoField()
   # votes_total = models.IntegerField(default=1)
   # image = models.ImageField(upload_to='images/')
   # icon = models.ImageField(upload_to='images/')
@@ -13,7 +15,7 @@ class Teams(models.Model):
   description = models.TextField(default='Not Provided', max_length=2000)
   teammates = models.IntegerField(default=0)
   vacancy = models.IntegerField(default=1)
-  logged_in_user = models.ForeignKey(User, on_delete=models.CASCADE)
+  logged_in_user = models.ForeignKey(extendeduser, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.category
@@ -23,3 +25,7 @@ class Teams(models.Model):
 
   def pub_date_pretty(self):
     return self.pub_date.strftime('%b %e %Y')
+
+
+# class Teams(models.Model):
+#   teamup_id = models.ForeignKey(Teamup_recruitment_details, related_name='Teamup_recruitment_details', on_delete=models.CASCADE)
