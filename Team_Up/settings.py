cf9df5014157740fbbe5c551a4e95d36c1bd2182
ShivamBhirud <shivambhirudpr@gmail.com>
2021-02-26 21:35:57 +0530

    Deployment

--- Team_Up/settings.py
+++ Team_Up/settings.py
@@ -62,19 +62,19 @@ WSGI_APPLICATION = 'Team_Up.wsgi.application'
 # Database for DEV
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 ## Database In Production
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=os.environ.get('DATABASE_URL')
+#    )
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
