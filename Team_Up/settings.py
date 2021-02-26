abf1888abcc7f00e18a903ff4b52f2a2cde0ce95
ShivamBhirud <shivambhirudpr@gmail.com>
2021-02-26 18:18:00 +0530

    Deployment updates to settings

--- Team_Up/settings.py
+++ Team_Up/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # In Production
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'teamupp.herokuapp.com']
 
