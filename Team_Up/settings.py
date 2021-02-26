99bac35bf176c262f54eff0990a5a6a5035ef198
ShivamBhirud <shivambhirudpr@gmail.com>
2021-02-26 17:38:56 +0530

    Deployment updates to settings

--- Team_Up/settings.py
+++ Team_Up/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 
 # STATICFILES_DIRS = [
